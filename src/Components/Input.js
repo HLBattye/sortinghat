db07ba59386c83f2ce8afb6d5ae18bca225f807e
hlbattye <helen.battye@hotmail.com>
2021-03-24 10:00:06 +0000

    updates

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -5,7 +5,6 @@ class Input extends React.Component {
     constructor(props) {
         super(props);
         this.state = { value: '' };
-
         this.handleChange = this.handleChange.bind(this);
     }
     handleChange(event) {
